927748c0cd9e3785327d8143275b3a09d5802578
luwei <lw1023@gmail.com>
2016-09-01 08:53:38 +0800

    Add ScopeBarSimple Project

---
+++ ScopeBar/ScopeBar/ScopeBar.swift
@@ -0,0 +1,21 @@
+//
+//  ScopeBar.swift
+//  ScopeBar
+//
+//  Created by 陆伟 on 16/9/1.
+//  Copyright © 2016年 luwei. All rights reserved.
+//
+
+import UIKit
+
+class ScopeBar: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ScopeBarSimple/ScopeBarSimple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScopeBarSimple
+//
+//  Created by 陆伟 on 16/9/1.
+//  Copyright © 2016年 luwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
